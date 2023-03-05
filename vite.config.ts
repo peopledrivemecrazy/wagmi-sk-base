fd4b90ac7affa0f740aa1b6fbb267007e5ea63da
shriji <shrijike@gmail.com>
2023-03-05 11:55:26 -0500

    chonky upate to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-
+import inject from '@rollup/plugin-inject';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		inject({
+			util: 'util/'
+		})
+	],
+	build: {
+		rollupOptions: {
+			plugins: [nodePolyfills()]
+		},
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	},
+
 	optimizeDeps: {
 		esbuildOptions: {
 			define: {
@@ -11,9 +26,20 @@ export default defineConfig({
 			},
 			plugins: [
 				NodeGlobalsPolyfillPlugin({
-					buffer: true
+					buffer: true,
+					process: true
 				})
 			]
 		}
 	}
+	// resolve: {
+	// 	alias: {
+	// 		Buffer: 'vite-compatible-readable-buffer'
+	// 		// stream: 'vite-compatible-readable-stream',
+	// 		// process: 'process/browser',
+	// 		// stream: 'stream-browserify',
+	// 		// zlib: 'browserify-zlib',
+	// 		// util: 'util'
+	// 	}
+	// }
 });
